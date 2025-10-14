dfbcce5ace1271e842e00de0ca23d013d274863d
abhinav singh <65818633+kunwarabhinav123@users.noreply.github.com>
2025-10-15 00:05:57 +0530

    NetflixGPT: Complete React app with Firebase auth and TMDB integration
    Features:
    - Firebase Authentication (sign in/sign up)
    - Redux state management
    - TMDB API integration for movies
    - OpenAI GPT integration
    - Responsive design with Tailwind CSS
    - React Router for navigation
    - Environment variables for API keys
    - Firebase hosting deployment
    
    Security:
    - API keys stored in environment variables
    - .env file excluded from git
    - GitHub push protection enabled

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export const LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const AVATAR = "https://occ-0-2483-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTvTV1d97HoOuutIG9GUEJgNIhg89JU3EQmtIikzdqolTLHSDqxwytfl61TC-HlrVt7QrzxdB5xR3nD2CPKNL9TF3qKTmcI.png?r=cad";
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_KEY}`
+  }
+};
+
+export const IMG_CDN_URL="https://image.tmdb.org/t/p/w500/";
+
+export const BG_URL="https://assets.nflxext.com/ffe/siteui/vlv3/cb72daa5-bd8d-408b-b949-1eaef000c377/web/IN-en-20250825-TRIFECTA-perspective_a3209894-0b01-4ddb-b57e-f32165e20a3f_small.jpg";
+
+export const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
